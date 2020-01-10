ae842d8deb2c072fa0aac2ce242501c1c913a2bc
kyoto <kyoto2514@gmail.com>
2020-01-10 09:25:53 +0900

    詳細：歩数系以外できた

--- app/src/main/java/com/example/asaco2/ui/home/BottomSheetFragment.kt
+++ app/src/main/java/com/example/asaco2/ui/home/BottomSheetFragment.kt
@@ -10,16 +10,31 @@ import kotlinx.android.synthetic.main.fragment_included.*
 
 
 class BottomSheetFragment(
-    private val element: List<CalendarEntity>,
+    private val list: List<CalendarEntity>,
     private val dayString: String
 ) : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_included,container,false)
+    ): View? = inflater.inflate(R.layout.fragment_included, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         nowText.text = dayString
+        var food = ""
+        var calory = ""
+        var total = 0
+        for (element in list) {
+            if (element == list[1]) {
+                food = element.food.toString()
+                calory = "${element.absorption}cal"
+            } else {
+                food += "\n${element.food}"
+                calory += "\n${element.absorption}cal"
+            }
+            total += element.absorption as Int
+
+        }
+        sumText.text = "合計：${total}cal"
     }
 }
